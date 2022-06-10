a7ee99ae3e94033be12d11f6a0d88c3316032cc8
Crendgrim <Crendgrim@users.noreply.github.com>
2022-06-10 21:52:19 +0200

    Initial Commit, split Arcanus compat to new mod

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dynamic Crosshair - Mod Compatibility
+
+Dynamic Crosshair is a Minecraft mod for the Fabric launcher.
+It hides or changes the crosshair dependent on context.
+
+This is an add-on that adds compatibility for various mods.
+
---
+++ src/main/java/mod/crend/dynamiccrosshair/compat/DynamicCrosshairCompat.java
@@ -0,0 +1,10 @@
+package mod.crend.dynamiccrosshair.compat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DynamicCrosshairCompat implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}

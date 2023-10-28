3cd03294be13be11040345ba9668a3a69089b0e5
Crendgrim <Crendgrim@users.noreply.github.com>
2023-10-28 10:14:54 +0200

    3.5: Update compat for GuardVillagers

--- src/main/java/mod/crend/dynamiccrosshair/compat/guardvillagers/ApiImplGuardVillagers.java
+++ src/main/java/mod/crend/dynamiccrosshair/compat/guardvillagers/ApiImplGuardVillagers.java
@@ -1,8 +1,8 @@
 package mod.crend.dynamiccrosshair.compat.guardvillagers;
 
-import dev.mrsterner.guardvillagers.GuardVillagers;
-import dev.mrsterner.guardvillagers.GuardVillagersConfig;
-import dev.mrsterner.guardvillagers.common.entity.GuardEntity;
+import dev.sterner.guardvillagers.GuardVillagers;
+import dev.sterner.guardvillagers.GuardVillagersConfig;
+import dev.sterner.guardvillagers.common.entity.GuardEntity;
 import mod.crend.dynamiccrosshair.api.CrosshairContext;
 import mod.crend.dynamiccrosshair.api.DynamicCrosshairApi;
 import mod.crend.dynamiccrosshair.component.Crosshair;
@@ -34,15 +34,11 @@ public class ApiImplGuardVillagers implements DynamicCrosshairApi {
 	public Crosshair computeFromEntity(CrosshairContext context) {
 		Entity entity = context.getEntity();
 
-		if (entity instanceof GuardEntity guard) {
-			boolean configValues = !GuardVillagersConfig.giveGuardStuffHOTV
-					|| !GuardVillagersConfig.setGuardPatrolHotv
-					|| context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE) && GuardVillagersConfig.giveGuardStuffHOTV
-					|| context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE) && GuardVillagersConfig.setGuardPatrolHotv
-					|| context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE) && GuardVillagersConfig.giveGuardStuffHOTV
-					&& GuardVillagersConfig.setGuardPatrolHotv;
-			boolean inventoryRequirements = !context.player.shouldCancelInteraction() && guard.isOnGround();
-			if (configValues && inventoryRequirements && guard.getTarget() != context.player && guard.canMoveVoluntarily()) {
+		if (entity instanceof GuardEntity guard && !context.player.shouldCancelInteraction() && guard.getTarget() != context.player && guard.canMoveVoluntarily()) {
+			if (context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE)
+					|| guard.getPlayerEntityReputation(context.player) >= GuardVillagersConfig.reputationRequirement
+					|| guard.getOwnerId() != null && guard.getOwnerId().equals(context.player.getUuid())
+			) {
 				return Crosshair.INTERACTABLE;
 			}
 		}
@@ -52,8 +48,8 @@ public class ApiImplGuardVillagers implements DynamicCrosshairApi {
 			if (entity instanceof VillagerEntity villagerEntity) {
 				if (!villagerEntity.isBaby()
 						&& (villagerEntity.getVillagerData().getProfession() == VillagerProfession.NONE || villagerEntity.getVillagerData().getProfession() == VillagerProfession.NITWIT)
-						&& (!GuardVillagersConfig.ConvertVillagerIfHaveHOTV || context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE)
-						&& GuardVillagersConfig.ConvertVillagerIfHaveHOTV)) {
+						&& (!GuardVillagersConfig.convertVillagerIfHaveHotv || context.player.hasStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE))
+				) {
 					return Crosshair.USABLE;
 				}
 			}
